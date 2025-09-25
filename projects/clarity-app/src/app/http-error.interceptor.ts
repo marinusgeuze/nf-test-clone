42d761c094a439a65dac927cfbd769910d7d36f0
Marinus Geuze <marinus.geuze@pinkroccade.nl>
2025-09-25 16:54:00 +0200

    Development

---
+++ projects/clarity-app/src/app/http-error.interceptor.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    console.log('HTTP Request intercepted:', request.url);
+
+    return next.handle(request);
+  }
+}
